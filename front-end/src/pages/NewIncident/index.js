fb17ff201b1b13ed13020aa5ca6fb609036eaa5f
Alfredo Henrique <alfredohenrique@alu.ufc.br>
2020-03-26 21:39:15 -0300

    frontend created

---
+++ front-end/src/pages/NewIncident/index.js
@@ -0,0 +1,74 @@
+import React, {useState} from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
+
+import logoImg from '../../assets/logo.svg';
+
+import './styles.css';
+
+export default function NewIncident() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+
+  const  ongId = localStorage.getItem('ongId');
+  const history = useHistory();
+
+  async function handleNewIncident(event){
+    event.preventDefault();
+
+    const data = { title, description, value };
+
+    try {
+      await api.post('/incidents', data, {
+        headers: {
+          Authorization: ongId,
+        }
+      });
+
+      history.push('/profile');
+    } catch (error) {
+      alert('Erro ao tentar cadastrar caso, tente novamente.')
+    }
+  }
+  
+  return (
+    <div className="new-incident-container">
+          <div className="content">
+              <section>
+                  <img src={logoImg} alt=""/>
+
+                  <h1>Cadastrarnovo caso</h1>
+                  <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+
+                  <Link className="back-link" to="/">
+                    <FiArrowLeft size={16} color="#E02041"/>
+                    Não tenho cadastro
+                </Link>
+              </section>
+
+              <form>
+                  <input 
+                    placeholder="Título do caso" 
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    />
+                  <textarea 
+                    placeholder="Descrição" 
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+                    />
+                  <input 
+                    placeholder="Valor em reais" 
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                    />
+
+                  <button onClick={handleNewIncident} className="button">Cadastrar</button>
+              </form>
+          </div>
+      </div>
+  );
+}
